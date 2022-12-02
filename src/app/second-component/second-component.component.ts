8fd422d44541539c88816692a6fea6b1dcb04aa1
seanbh <seanbh@users.noreply.github.com>
2022-12-02 14:51:12 -0600

    wip

--- src/app/second-component/second-component.component.ts
+++ src/app/second-component/second-component.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-second-component',
   templateUrl: './second-component.component.html',
-  styleUrls: ['./second-component.component.scss']
+  styleUrls: ['./second-component.component.scss'],
 })
-export class SecondComponentComponent {
+export class SecondComponentComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject();
+  name = '';
 
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.activatedRoute.queryParams.subscribe((params) => {
+      this.name = params['name'];
+      console.log(this.name);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
+  }
 }
